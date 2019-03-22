1fd1dd0ff3f25d15edf78d971aaba2b0f518abba
nadeeraka <nimanthanadeeraka@outlook.com>
2019-03-22 21:11:33 +0530

    navbar style added

--- src/components/layouts/nav.js
+++ src/components/layouts/nav.js
@@ -1,61 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const Nav = () => (
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">
-      Navbar
-    </a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            Link
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            Link
-          </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">
-              Action
-            </a>
-            <a class="dropdown-item" href="#">
-              Another action
-            </a>
-            <div class="dropdown-divider" />
-            <a class="dropdown-item" href="#">
-              Something else here
-            </a>
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
+          <div className="container">
+            <Link to="/" className="navbar-brand">
+              ClintPanel
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggler="collapse"
+              data-target="#navbarMain"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
           </div>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+        </nav>
+      </div>
+    );
+  }
+}
 export default Nav;
