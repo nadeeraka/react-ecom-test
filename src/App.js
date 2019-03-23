286ab4596b72681391471e52a4579acfb2a47dd8
nadeeraka <nimanthanadeeraka@outlook.com>
2019-03-23 13:05:05 +0530

    new component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Nav from "./components/layouts/nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./components/layouts/Dashboard";
 
 class App extends Component {
   render() {
@@ -10,7 +11,9 @@ class App extends Component {
         <div>
           <Nav />
           <div className="container">
-            <h1 className="h">Welcome to Califonia</h1>
+            <Switch>
+              <Route to="/" exact component={Dashboard} />
+            </Switch>
           </div>
         </div>
       </Router>
